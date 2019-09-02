714c5062aa650b05b691dfc5b4b87c3413bf1e32
Antti Karvanen <antti.karvanen@exove.fi>
2019-09-02 16:17:47 +0300

    first version

---
+++ oauth-react/src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+	const [ auth, setAuth ] = useState(false)
+	const [ userName, setUserName ] = useState('')
+	 
+	const clientId = '41e1a75acc0e9d6cb3c5'
+	const redirectUri = 'http://localhost:3001/login'
+	const loginUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`
+
+	useEffect(() => {
+		const authorize = async () => {
+			const token = sessionStorage.getItem('token')
+			const response = await axios.get('http://localhost:3001/auth', {
+				headers: {
+					authorization: token
+				}
+			})
+
+			const { auth, userName } = response.data
+
+			setAuth(auth)
+			setUserName(userName)
+		}
+		authorize()
+	}, [])
+
+	const logOut = () => {
+		sessionStorage.clear()
+		setAuth(false)
+	}
+
+	if (auth) {
+		return (
+			<div>
+				<p>You are {userName}</p>
+				<button onClick={logOut}>Log out</button>
+			</div>
+		)
+	} else {
+		return (
+			<div>
+				<p>You are not logged in</p>
+				<a href={loginUrl}>Log in</a>
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,106 @@
+'use strict';
+require('dotenv').config()
+
+const Hapi = require('hapi')
+const Path = require('path')
+const Inert = require('inert')
+const axios = require('axios')
+const jwt = require('jsonwebtoken')
+
+
+const server = Hapi.server({
+    host: '0.0.0.0',
+    port: process.env.PORT
+})
+
+
+const start =  async function() {
+  await server.register(Inert)
+
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+      directory: {
+        path: Path.join(__dirname, './oauth-react/build/'),
+        listing: false,
+        index: true
+      }
+    }
+  })
+
+	server.route({
+		method:'GET',
+		path:'/auth',
+		handler: async (request, h) => {
+			let auth = false
+			let userName = ''
+			try {
+				const token = request.headers.authorization
+				const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+				auth = true
+				userName = decodedToken.login
+			} catch(error) {
+				console.log(error)
+			}
+
+			const response = {
+				auth,
+				userName
+			}
+			return response
+		}
+	});
+
+  server.route({
+    method:'GET',
+		path:'/login',
+		handler: async (request, h) => {
+
+			const clientId = process.env.CLIENT_ID
+			const clientSecret = process.env.CLIENT_SECRET
+
+			const { code } = request.query
+			console.log({ code })
+
+			const response = await axios.post(`https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`)
+
+			console.log(response.data)
+			const token = response.data.split('&')[0].split('=')[1]
+
+			const githubUser = await axios.get(`https://api.github.com/user?client_id=${clientId}&client_secret=${clientSecret}`, {
+				headers: {
+					Authorization: `token ${token}`,
+					'User-Agent': 'oauth-demo'
+				}
+			})
+
+			console.log(githubUser.data)
+			const { id, login } = githubUser.data
+			const userForToken = {
+				id,
+				login
+			}
+			const jwtToken = jwt.sign(userForToken, process.env.JWT_SECRET)
+
+			console.log({jwtToken})
+
+			return (
+				`<script>const main = async () => {await sessionStorage.setItem('token', '${jwtToken}');window.location.href = 'http://localhost:3001'};main();</script>`
+			)
+    }
+
+  })
+
+  try {
+    await server.start()
+  }
+  catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+
+  console.log('Server running at:', server.info.uri)
+}
+
+start()
